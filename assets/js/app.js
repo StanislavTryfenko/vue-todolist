004a143aace685ce7d62eb3bbe6adcc7cf15fa5b
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-03-13 19:43:04 +0100

    basic to do list

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,23 @@ const { createApp } = Vue
 createApp({
     data(){
         return{
-
+            tasksList:[],
+            inputTask: {},
+            newTask: '',
         }
     },
     methods: {
-        
+        takeTask(){
+            if (this.newTask.length > 0 ) {
+                this.tasksList.push({
+                    text: this.newTask,
+                    done: false,
+                });  
+            };
+            this.newTask = '';
+        },
+        toggleTask(index){
+            this.tasksList.splice(index, 1)
+        },
     },
 }).mount('#app')
